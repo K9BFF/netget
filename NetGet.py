4f7bac344a5e0f30d5f64223c843ac5e1c759330
K9BFF <masoncodes0@gmail.com>
2023-04-16 23:24:47 -0500

    bug fixes

--- NetGet.py
+++ NetGet.py
@@ -1,4 +1,4 @@
-#   Copyright 2017 masoncodes
+#   Copyright 2017-2023 masoncodes
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -18,7 +18,22 @@ if os.name == 'nt':
 # get proxies
 print("You are connected to the following proxies:", urllib.request.getproxies())
 
-def getinfo():
+#asks user if they want to retry
+def retry():
+    print ("Get info on another URL? (Y/n)")
+    another = input(">> ")
+    if another == 'n':
+        exit()
+
+    if another == "":
+        getinfo()
+
+    if another == "y":
+        getinfo()
+    else:
+            print("Huh?")
+
+def getinfo(): #the actual meat of the program
     while 1 > 0:  # loop
         # url input
         print("Please input a URL. (ex. \"google.com.\" Do not include \"http(s)://\" in the URL.)")
@@ -38,34 +53,33 @@ def getinfo():
         try:  # open connection
             conn = urllib.request.urlopen(url)
             purl = urlparse(url)
-            print("Trying", url, "on port", purl.port, "using", purl.scheme+"...")
+            print("Trying", a, "on port", purl.port, "using", purl.scheme+"...")
             global r1
 
+        #catch http error
         except urllib.error.HTTPError as e:  # http error exception (404, 500, etc)
             print("Error:", url+"'s error code was", e.code)
             errcode = str(e.code)
             print("If you would like to know what this code means, click here: https://httpstatuses.com/"+errcode)
+            break
 
+        #connection refused
         except urllib.error.URLError as e:  # dns/connection refused error (FUCKED UP FUCKED UP FUCKED UP FUCKED UP YOU DONE FUCKED UP -XXXTentacion, Take a step back)
             print('Error: Connection refused. Make sure you didn\'t mistype the URL, and that you are not connecting to'
                   ' localhost.')
+            break
 
         else:  # connction successful
-            print(url+"'s status code was", conn.getcode())
+            print(s+"://"+a+"'s status code was", conn.getcode())
             good_code = str(conn.getcode())
             print("If you would like to know what this code means, click here: https://httpstatuses.com/"+good_code)
-            print("Would you like to see the header from", a+"? (y/n)")
+            print("Would you like to see the header from", a+"? (y/N)")
             getHeader = input(">> ").lower()
             if getHeader == 'y':
                 print(conn.info())  # print header
+                retry()
             else:
-                pass
+                retry()
 
 getinfo()
-
-print("Get info on another URL? (y/n)")  # continue or break loop
-another = input(">> ")
-if another == 'y':
-    pass
-else:
-    exit()
+retry()
